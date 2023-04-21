afb40d05634ff37c6f512bdc34c6c711bf05f7ed
Ezequiel <ezebriones01@gmail.com>
2023-04-21 17:11:33 -0300

    last commit, finishied application

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Requires the inquirer functions, tasks and colours
 const { guardarDB, leerDB } = require("./helpers/guardarArchivo");
-const { inquirerMenu, pausa, leerInput, listadoTareasBorrar, confirmar } = require("./helpers/inquirer");
+const { inquirerMenu, pausa, leerInput, listadoTareasBorrar, confirmar, mostrarListadoChecklist } = require("./helpers/inquirer");
 const Tareas = require("./models/tareas");
 require("colours");
 
@@ -19,23 +19,29 @@ const main = async () => {
     opt = await inquirerMenu();
 
     switch (opt) {
-      case "1":
+      case "1": // Create new task
         const desc = await leerInput("Descripcion:");
         tareas.crearTarea(desc);
         break;
 
-      case "2":
+      case "2": // List all tasks
         tareas.listadoCompleto();
         break;
-      case "3":
+      case "3": // Show completed tasks
         console.log("Aca van las tareas completadas");
         tareas.listarPendientesCompletadas();
         break;
-      case "4":
+      case "4": // Show pending tasks
         console.log("Aca van las tareas pendientes");
         tareas.listarPendientesCompletadas(false);
         break;
-      case "6":
+      case "5": // Complete tasks
+        const ids = await mostrarListadoChecklist(tareas.listadoArr);
+        tareas.toggleCompletadas(ids);
+        console.log(ids);
+
+        break;
+      case "6": // Delete tasks
         const id = await listadoTareasBorrar(tareas.listadoArr);
         if (id !== "0") {
           const ok = await confirmar("Seguro maquina?".red);
--- models/tareas.js
+++ models/tareas.js
@@ -58,13 +58,28 @@ class Tareas {
         // show complete tasks
         if (completadoEn) {
           contador += 1;
-          console.log(`${(contador + ".").green} ${desc} :: ${estado}`);
+          console.log(`${(contador + ".").green} ${desc} :: ${completadoEn.green}`);
         }
       } else {
         if (!completadoEn) {
           contador += 1;
           console.log(`${(contador + ".").green} ${desc} :: ${estado}`);
         }
+      }
+    });
+  }
+
+  toggleCompletadas(ids = []) {
+    ids.forEach((id) => {
+      const tarea = this._listado[id];
+      if (!tarea.completadoEn) {
+        tarea.completadoEn = new Date().toISOString();
+      }
+    });
+
+    this.listadoArr.forEach((tarea) => {
+      if (!ids.includes(tarea.id)) {
+        this._listado[tarea.id].completadoEn = null;
       }
     });
   }
